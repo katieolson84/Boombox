4a51f7216368899adbbd53a2b7ba4133d0b02da3
katieolson84 <katieolson84@gmail.com>
2021-04-01 13:43:51 -0600

    initial setup

---
+++ boombox/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// Import Styles
+import './styles/app.scss';
+// Components
+import Player from './components/Player';
+import Song from './components/Song';
+
+function App() {
+  return (
+    <div className="App">
+      <Song />
+      <Player />
+    </div>
+  );
+}
+
+export default App;
